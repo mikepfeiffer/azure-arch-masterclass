273c6c3e906e51eb97f496dfc8f4efc96ee464d9
Mike Pfeiffer <mike.pfeiffer@gmail.com>
2019-05-22 11:34:10 -0700

    Add files via upload

---
+++ DemoRunbook.ps1
@@ -0,0 +1,34 @@
+﻿<#
+    .DESCRIPTION
+        An example runbook which gets all the ARM resources using the Run As Account (Service Principal)
+
+    .NOTES
+        AUTHOR: Azure Automation Team
+        LASTEDIT: Mar 14, 2016
+#>
+
+$connectionName = "AzureRunAsConnection"
+try
+{
+    # Get the connection "AzureRunAsConnection "
+    $servicePrincipalConnection=Get-AutomationConnection -Name $connectionName         
+
+    "Logging in to Azure..."
+    Add-AzureRmAccount `
+        -ServicePrincipal `
+        -TenantId $servicePrincipalConnection.TenantId `
+        -ApplicationId $servicePrincipalConnection.ApplicationId `
+        -CertificateThumbprint $servicePrincipalConnection.CertificateThumbprint 
+}
+catch {
+    if (!$servicePrincipalConnection)
+    {
+        $ErrorMessage = "Connection $connectionName not found."
+        throw $ErrorMessage
+    } else{
+        Write-Error -Message $_.Exception
+        throw $_.Exception
+    }
+}
+
+Start-AzureRmVM -ResourceGroupName ADDS -Name ADVM
